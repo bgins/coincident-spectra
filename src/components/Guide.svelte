1b87bb6df3fb1bfd1580f95f5a8dc263150c67fa
Brian Ginsburg <gins@brianginsburg.com>
2022-02-20 15:58:31 -0800

    Add initial version of guide

---
+++ src/components/Guide.svelte
@@ -0,0 +1,43 @@
+<div class="grid grid-flow-row auto-rows-max gap-6">
+  <div>
+    <h2 class="text-2xl pb-2">Of Harmonics and Spectra</h2>
+    <p>
+      Coincident Spectra is an additive synthesizer that generates
+      sounds from 16 sine wave 'partials' that can be switched between
+      Harmonics (integer multiples of the fundamental frequency) and
+      Spectra, which remaps the frequency ratios of the harmonic series
+      such that the partial tones are coincident with the selected equal
+      temperament. Coincident spectra reduce sensory dissonance and
+      beating between the timbre and tuning rendering chords more in
+      tune.
+    </p>
+  </div>
+  <div class="grid grid-flow-row auto-rows-max gap-2">
+    <h2 class="text-2xl">Listen and Compare</h2>
+    <ul class="list-disc list-inside">
+      <li>Select Start Audio to activate the instrument</li>
+      <li>Select Keyboard or a MIDI controller</li>
+      <li>Play notes and chords in Harmonics mode</li>
+      <li>Switch to Spectra mode. Listen and compare.</li>
+    </ul>
+    <p>
+      Play
+      <kbd class="kbd kbd-sm">u</kbd>,
+      <kbd class="kbd kbd-sm">[</kbd>,
+      <kbd class="kbd kbd-sm">2</kbd>
+      in 12-tone equal temperament for C-major chord on your computer keyboard.
+    </p>
+    <p>
+      MIDI is available in Chrome and Chromium-based browsers, and it
+      will likely be in Firefox soon.
+    </p>
+  </div>
+
+  <div class="grid grid-flow-row auto-rows-max gap-2">
+    <h2 class="text-2xl">Adjust Timbre</h2>
+    <p>
+      The gain sliders control the amplitude of each partial. Adjusting
+      them changes to timbre of the instrument.
+    </p>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,13 +7,17 @@
   import { EventEmitter } from '$lib/common/event-emitter'
   import partials from '$lib/audio/partials.json'
   import { audioStore, drawbars, midiInputs, midiStatus } from '../stores'
+  import Guide from '$components/Guide.svelte'
+
+  type View = 'instrument' | 'info'
 
   const noteEmitter = new EventEmitter()
   const keyboard = new Keyboard()
   const midi = new Midi()
   const synth = new AdditiveSynth()
-  let isMobileDevice: boolean = false
   let selectedPartials = 'harmonics'
+  let view: View = 'instrument'
+  let isMobileDevice: boolean = false
 
   keyboard.enable(noteEmitter)
 
@@ -27,6 +31,14 @@
     } else {
       isMobileDevice = false
     }
+  }
+
+  const showInfo = () => {
+    view = 'info'
+  }
+
+  const hideInfo = () => {
+    view = 'instrument'
   }
 
   const startAudio = () => {
@@ -82,94 +94,115 @@
 <div
   class="grid grid-flow-row auto-rows-max justify-center bg-neutral h-screen p-10 text-base-content"
 >
-  <div class="card w-full bg-base-100 shadow-xl">
+  <div class="card bg-base-100 shadow-xl" style="width: 950px">
     <div class="card-body">
-      <h1 class="text-4xl pb-7">Coincident Spectra</h1>
-      <div class="grid grid-flow-row auto-rows-max gap-7">
-        <div class="grid grid-flow-col auto-cols-max gap-4">
-          {#if $audioStore.contextState === 'running'}
-            <button class="btn btn-primary" on:click={pauseAudio}>
-              Pause Audio
-            </button>
-          {:else}
-            <button class="btn btn-primary" on:click={startAudio}>
-              Start Audio
-            </button>
-          {/if}
-          <select class="select w-full max-w-xs select-primary">
-            <option disabled selected>Tuning System</option>
-            <option value="12-ed2">12-ED2</option>
-            <option value="19-ed2">19-ED2</option>
-          </select>
-          <select
-            class="select w-full max-w-xs select-primary"
-            bind:value={selectedPartials}
-          >
-            <option value="harmonics">Harmonics</option>
-            <option value="spectra">Spectra</option>
-          </select>
-          <select
-            class="select w-full max-w-xs select-primary"
-            on:change={setController}
-          >
-            <option>Keyboard</option>
-            {#if $midiStatus !== 'unavailable'}
-              <option>MIDI</option>
+      <div class="grid grid-flow-col grid-cols-2 align-center pb-6">
+        <h1 class="text-4xl" style="display: inline-block">
+          Coincident Spectra
+        </h1>
+        <div class="grid justify-end items-center pt-1 pr-2">
+          <button>
+            {#if view === 'instrument'}
+              <div class="tooltip" data-tip="Show Info" on:click={showInfo}>
+                <img src="information.svg" alt="Show information" />
+              </div>
+            {:else}
+              <div on:click={hideInfo} class="pb-1">
+                <img src="close.svg" alt="Hide information" />
+              </div>
             {/if}
-          </select>
-          {#if $midiStatus === 'enabled'}
+          </button>
+        </div>
+      </div>
+      {#if view === 'instrument'}
+        <div class="grid grid-flow-row auto-rows-max gap-7">
+          <div class="grid grid-flow-col auto-cols-max gap-4">
+            {#if $audioStore.contextState === 'running'}
+              <button class="btn btn-primary" on:click={pauseAudio}>
+                Pause Audio
+              </button>
+            {:else}
+              <button class="btn btn-primary" on:click={startAudio}>
+                Start Audio
+              </button>
+            {/if}
+            <select class="select w-full max-w-xs select-primary">
+              <option disabled selected>Tuning System</option>
+              <option value="12-ed2">12-ED2</option>
+              <option value="19-ed2">19-ED2</option>
+            </select>
             <select
               class="select w-full max-w-xs select-primary"
-              on:change={setMidiInput}
+              bind:value={selectedPartials}
             >
-              {#each Object.keys($midiInputs) as midiInput}
-                <option>{midiInput}</option>
-              {/each}
+              <option value="harmonics">Harmonics</option>
+              <option value="spectra">Spectra</option>
             </select>
-          {:else if $midiStatus === 'unavailable'}
-            <div class="tooltip" data-tip="MIDI unavailable in this browser">
+            <select
+              class="select w-full max-w-xs select-primary"
+              on:change={setController}
+            >
+              <option>Keyboard</option>
+              {#if $midiStatus !== 'unavailable'}
+                <option>MIDI</option>
+              {/if}
+            </select>
+            {#if $midiStatus === 'enabled'}
+              <select
+                class="select w-full max-w-xs select-primary"
+                on:change={setMidiInput}
+              >
+                {#each Object.keys($midiInputs) as midiInput}
+                  <option>{midiInput}</option>
+                {/each}
+              </select>
+            {:else if $midiStatus === 'unavailable'}
+              <div class="tooltip" data-tip="MIDI unavailable in this browser">
+                <select disabled class="select w-full max-w-xs select-primary">
+                  <option disabled selected>MIDI Device</option>
+                </select>
+              </div>
+            {:else}
               <select disabled class="select w-full max-w-xs select-primary">
                 <option disabled selected>MIDI Device</option>
               </select>
-            </div>
-          {:else}
-            <select disabled class="select w-full max-w-xs select-primary">
-              <option disabled selected>MIDI Device</option>
-            </select>
-          {/if}
-        </div>
-        <div class="overflow-x-auto">
-          <table class="table table-compact w-full">
-            <thead>
-              <tr>
-                <th>Partial</th>
-                <th>Harmonics</th>
-                <th>Spectra</th>
-                <th>Gain</th>
-              </tr>
-            </thead>
-            <tbody>
-              {#each partials.harmonics as harmonic, index}
+            {/if}
+          </div>
+          <div class="overflow-x-auto">
+            <table class="table table-compact w-full">
+              <thead>
                 <tr>
-                  <th>{index + 1}</th>
-                  <td>{harmonic}</td>
-                  <td>{partials.spectra[index]}</td>
-                  <td>
-                    <input
-                      type="range"
-                      min="0"
-                      max="100"
-                      value={($drawbars[index] * 1000).toString()}
-                      class="range range-xs"
-                      on:input={event => setDrawbar(index, event)}
-                    />
-                  </td>
+                  <th>Partial</th>
+                  <th>Harmonics</th>
+                  <th>Spectra</th>
+                  <th>Gain</th>
                 </tr>
-              {/each}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {#each partials.harmonics as harmonic, index}
+                  <tr>
+                    <th>{index + 1}</th>
+                    <td>{harmonic}</td>
+                    <td>{partials.spectra[index]}</td>
+                    <td>
+                      <input
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={($drawbars[index] * 1000).toString()}
+                        class="range range-xs"
+                        on:input={event => setDrawbar(index, event)}
+                      />
+                    </td>
+                  </tr>
+                {/each}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      {:else}
+        <Guide />
+      {/if}
     </div>
   </div>
 </div>
