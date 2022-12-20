8d1373a29a0dfd6395249f4135de68a6726e960f
Brian Ginsburg <7957636+bgins@users.noreply.github.com>
2022-12-20 10:43:27 -0800

    Upgrade to SvelteKit 1.0.0 stable release (#7)
    * Add adapter static fallback route
    
    * Extend SvelteKit tsconfig and update path aliases
    
    * Update rendering configs
    
    * Update to tailwind 3.0.0 config
    
    * Upgrade to SvelteKit 1.0.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
-    options: {
-      safelist: [/data-theme$/],
-    },
-  },
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   plugins: [require("@tailwindcss/typography"),require("daisyui")],
   daisyui: {
     styled: true,
