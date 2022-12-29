4a5647688a569566b73ee72bb6cfcf8043528cbd
Brian Ginsburg <gins@brianginsburg.com>
2022-12-29 11:13:25 -0800

    Add partials selection control

--- src/components/Controls.svelte
+++ src/components/Controls.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Synth } from '$lib/audio/audio'
+  import Partials from '$components/controls/Partials.svelte'
   import Tuning from '$components/controls/Tuning.svelte'
 
   export let synth: Synth
@@ -8,13 +9,8 @@
 <div class="grid grid-flow-row auto-rows-max gap-7">
   <div class="grid grid-flow-col auto-cols-max gap-4">
     <button class="btn btn-primary">Start Audio</button>
-
-    <div class="btn-group">
-      <button class="btn btn-outline btn-active">Harmonics</button>
-      <button class="btn btn-primary btn-outline">Spectra</button>
-    </div>
-
+    <Partials {synth} />
     <button class="btn btn-primary btn-outline">Keyboard</button>
-    <Tuning {synth}/>
+    <Tuning {synth} />
   </div>
 </div>
---
+++ src/components/controls/Partials.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { Synth } from '$lib/audio/audio'
+
+  import { isPartials } from '$lib/audio/partials'
+  import { partials } from '../../stores'
+  import { setPartialsTable } from '$lib/audio/additive-synth'
+
+  export let synth: Synth
+
+  const setPartials = (event: {
+    type: string
+    currentTarget: HTMLButtonElement
+    code?: string
+  }) => {
+    if (event.type === 'keypress' && event.code !== 'Enter') {
+      return
+    }
+
+    const { textContent } = event.currentTarget
+    const selected = textContent.toLowerCase()
+
+    if (isPartials(selected)) {
+      partials.set(selected)
+      setPartialsTable()
+      synth.updateParams()
+    } else {
+      console.error('Invalid partials selection: ', selected)
+    }
+  }
+</script>
+
+<div class="btn-group">
+  <button
+    class="btn btn-primary btn-outline"
+    class:btn-active={$partials === 'harmonics'}
+    on:click={setPartials}
+    on:keypress={setPartials}
+    >Harmonics
+  </button>
+  <button
+    class="btn btn-primary btn-outline"
+    class:btn-active={$partials === 'spectra'}
+    on:click={setPartials}
+    on:keypress={setPartials}
+    >Spectra
+  </button>
+</div>
